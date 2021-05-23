d1a5cb8c959ed3885a15b66e3f3a9274391a9575
DewoneW536 <dwesterf@mail.depaul.edu>
2021-05-22 22:44:37 -0500

    Initial Commit

---
+++ AnimeAppV2/AnimeAppV2App.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeAppV2App.swift
+//  AnimeAppV2
+//
+//  Created by Dewone Westerfield on 5/22/21.
+//
+
+import SwiftUI
+
+@main
+struct AnimeAppV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
